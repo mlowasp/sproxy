e0491f6603127767c35b01cb260c5c0a000c1c75
Maxime Labelle <maxime@cognitif.ca>
2024-06-21 19:19:56 -0400

    .

---
+++ setup.py
@@ -0,0 +1,16 @@
+setup(
+    name='sproxy', 
+    version='1.0', 
+    author='Maxime Labelle',
+    author_email='maxime.labelle@owasp.org', 
+    description='sproxy is a high-performance socks4/socks5 proxy load-balancer',
+    scripts=['sproxy.py'],
+    data_files=[('/etc/systemd/system', ['sproxy.service']), ('/etc/sproxy', ['etc/sproxy.conf'])], 
+    install_requires=['PySocks','termcolor', 'configparser'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
---
+++ sproxy.1.md
@@ -0,0 +1,13 @@
+% sproxy(1) sproxy 1.0
+% Maxime Labelle
+% June 2024
+
+# NAME
+sproxy - high-performance socks4/socks5 proxy load-balancer
+
+# DESCRIPTION
+**sproxy** is a high-performance socks4/socks5 proxy load-balancer
+
+# EXAMPLES
+service sproxy start
+: start the service
