3428b9c54c1328f34672aa79722e9cf537dbf1ee
Maxime Labelle <maxime@cognitif.ca>
2024-06-22 08:56:04 -0400

    .

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,6 @@ cd src
 pyinstaller -F sproxy.py
 cd ..
 cp src/dist/sproxy sproxy_1.0-amd64/usr/bin/
-cp -r etc/sproxy/sproxy.conf sproxy_1.0-amd64/etc/sproxy/
-cp -r etc/sproxy.service sproxy_1.0-amd64/etc/systemd/system/
+cp -r src/etc/sproxy/sproxy.conf sproxy_1.0-amd64/etc/sproxy/
+cp -r src/etc/sproxy.service sproxy_1.0-amd64/etc/systemd/system/
 dpkg-deb --build --root-owner-group sproxy_1.0-amd64
